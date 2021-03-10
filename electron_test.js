7b7d6474e8f098a94a988712a0101bda806929cf
PriTexX <61851561+PriTexX@users.noreply.github.com>
2021-03-11 07:56:34 +1000

    Add files via upload

---
+++ electron_test.js
@@ -0,0 +1,52 @@
+const { app, ipcMain } = require("electron");
+const path = require("path");
+const Window = require("./plugins/Window.js")
+const Menu = require("./plugins/Menu.js")
+
+function create() {
+  const modalPath = path.join("file://", __dirname, "/html/index.html");
+  win = new Window(modalPath)
+  const menuTemplate = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Learn More'
+        },
+        {
+          label: 'Help'
+        },
+        {
+          label: 'Quit',
+          click(){
+            app.quit()
+          },
+          accelerator: 'Ctrl+Q'
+        }
+      ]
+    }
+  ]
+ // new Menu(menuTemplate)
+}
+
+ipcMain.on("create-new-window", (e, type) => {
+  if (type=='modal'){
+    modalPath = `file://${__dirname}/html/modal.html`
+    const options = {
+      width: 400,
+      height: 600,
+      maxWidth: 400,
+      maxHeight: 600,
+      minWidth: 400,
+      minHeight: 600,
+      modal: true,
+      parent: win,
+    }
+    let modal = new Window(modalPath, options)
+    menu = new Menu([], true)
+    //modal.setMenu(menu.modalMenu)
+  }
+})
+
+app.on("ready", create);
+app.on("all-window-closed", () => app.quit())
