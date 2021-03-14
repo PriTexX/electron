3128665c360e8bc9632bf751ed3c1f9b4b6210cf
PriTexX <61851561+PriTexX@users.noreply.github.com>
2021-03-14 17:38:57 +1000

    Delete scripts directory

--- scripts/Modal.js
+++
@@ -1,8 +0,0 @@
-const { ipcRenderer } = require("electron");
-
-const solveBtn = document.querySelector(".accept");
-const denyBtn = document.querySelector(".deny");
-
-denyBtn.addEventListener("click", () => {
-    ipcRenderer.send("modal-btn-clicked", {type: 'deny'})
-})
